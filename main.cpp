eeb85294b0d77ad7c816a1f07c148f176a0e6487
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-05-30 22:16:01 -0400

    remove code from main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include "Board.h"
-#include "Player.h"
 using namespace std;
 
 int main() {
-    Player* player = new Player('X');
-    Board board;
-
-    player->makeMove(board);
-    board.display();
-    player->makeMove(board);
-
-    board.display();
-
     cout << "Hello world!" << endl;
     return 0;
 }
