40552f3ded38c42f4441324e5c7ea2f78fe6441d
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-06-01 21:50:22 -0400

    create human player

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Player.h"
-#include "Utils.h"
 using namespace std;
 
 // constructor sin parametros
@@ -9,6 +8,7 @@ Player::Player() { }
 // constructor
 Player::Player(char token) {
 	this->token = token;
+    score = 0;
 }
 
 // obtener ficha
@@ -16,60 +16,14 @@ char Player::getToken() const {
 	return token;
 }
 
-// hacer movida
-void Player::makeMove(Board &board) {
-    // mostrar mensaje y salir en caso de estar el tablero lleno
-    if (board.isFull()) {
-        cout << "Tablero ya esta lleno" << endl;
-        return;
-    }
+// obtener puntuacion
+int Player::getScore() const {
+	return score;
+}
 
-    int column;
-    int row;
-    bool isRowValid = false;
-    bool isColumnValid = false;
-	bool isTokenAdded = false;
-    string label;
-
-    while(!isTokenAdded) {
-        // formar etiqueta para pedir fila
-        label = "Fila (";
-        label += getToken();
-        label += "): ";
-
-        while(!isRowValid) {
-            // pedir y leer fila
-            row = Utils::getNumber(label) - 1;
-            isRowValid = board.isRowInRange(row);
-
-            if (!isRowValid) {
-                cout << "Fila esta fuera de rango\n" << endl;
-            }
-        }
-
-        // formar etiqueta para pedir columna
-        label = "Columna (";
-        label += getToken();
-        label += "): ";
-
-        while(!isColumnValid) {
-            // pedir y leer columna
-            column = Utils::getNumber(label) - 1;
-            isColumnValid = board.isColumnInRange(column);
-
-            if (!isColumnValid) {
-                cout << "Columna esta fuera de rango\n" << endl;
-            }
-        }
-        
-        // intentar agregar ficha y obtener estado
-        isTokenAdded = board.addToken(row, column, getToken());
-        cout << endl;
-
-        // reiniciar valores si ficha no ha sido agregada
-        if (!isTokenAdded) {
-            isRowValid = false;
-            isColumnValid = false;
-        }
-    }
+// establecer puntuacion
+void Player::setScore(int score) {
+	if (score > 0) {
+		this->score = score;
+	}
 }
